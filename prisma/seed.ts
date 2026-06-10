71db6f4703fe88ea1126e0bbb4918cdc5bf3f054
Sonu <sonu.codeedinc@gmail.com>
2026-06-10 15:06:54 +0545

    fix:prisma version downgraded due to version conflict

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,8 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import "dotenv/config";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 async function main() {
   console.log("Seeding db");
